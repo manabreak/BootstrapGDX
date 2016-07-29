22088692b0f9515bb80b630150c86a87cee020ad
manabreak <morbidi.h@gmail.com>
2016-07-29 17:22:36 +0300

    Added JUnit support for the core project.

---
+++ core/test/me/manabreak/bootstrapgdx/DummyTest.java
@@ -0,0 +1,18 @@
+package me.manabreak.bootstrapgdx;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class DummyTest {
+    @Test
+    public void thisAlwaysPasses() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Ignore
+    public void thisIsIgnored() {
+    }
+}
